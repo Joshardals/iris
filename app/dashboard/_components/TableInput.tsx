6c8409d4f015e6c5dd995e8b07749e7c74992ae1
Joshardals <joshuabamidele219@gmail.com>
2024-07-31 13:12:19 +0100

    initial commit

--- app/dashboard/_components/TableInput.tsx
+++ app/dashboard/_components/TableInput.tsx
@@ -19,34 +19,32 @@ interface Props {
 export function TableInput({ caption, header, referredUsers }: Props) {
   const pathname = usePathname();
   return (
-    <div className="h-screen flex">
-      <Table className="bg-snow">
-        <TableCaption className="text-onyx/50">{caption}</TableCaption>
-        <TableHeader className="hover:bg-onyx/10">
-          <TableRow>
-            {header?.map((data, index) => {
-              return (
-                <TableHead
-                  className={`${index === 0 && "w-[100px]"}`}
-                  key={index}
-                >
-                  {data}
-                </TableHead>
-              );
-            })}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {pathname === "/dashboard/referrals" &&
-            referredUsers?.map((user, index) => (
-              <TableRow key={index} className="hover:bg-onyx/10">
-                <TableCell>{index + 1}</TableCell>
-                <TableCell>{user.email}</TableCell>
-                <TableCell>{user.username}</TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </div>
+    <Table className="bg-snow rounded-lg">
+      <TableCaption className="text-onyx/50">{caption}</TableCaption>
+      <TableHeader className="hover:bg-onyx/10">
+        <TableRow>
+          {header?.map((data, index) => {
+            return (
+              <TableHead
+                className={`${index === 0 && "w-[100px]"}`}
+                key={index}
+              >
+                {data}
+              </TableHead>
+            );
+          })}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {pathname === "/dashboard/referrals" &&
+          referredUsers?.map((user, index) => (
+            <TableRow key={index} className="hover:bg-onyx/10">
+              <TableCell>{index + 1}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.username}</TableCell>
+            </TableRow>
+          ))}
+      </TableBody>
+    </Table>
   );
 }
