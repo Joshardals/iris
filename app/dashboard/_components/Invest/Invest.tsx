26725507ee4f0d6553080a5b5c6acfbbd83e7984
Joshardals <joshuabamidele219@gmail.com>
2024-08-01 17:40:11 +0100

    initial commit

---
+++ app/dashboard/_components/Invest/Invest.tsx
@@ -0,0 +1,3 @@
+export default function Invest() {
+  return <div className="w-[20rem] bg-white h-[20rem] rounded-lg"></div>;
+}
--- app/dashboard/invest/page.tsx
+++ app/dashboard/invest/page.tsx
@@ -1,4 +1,18 @@
+import Invest from "../_components/Invest/Invest";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
-export default function InvestPage() {
-  return <div>Invest</div>;
+export default async function InvestPage() {
+  return (
+    <div className="space-y-5 md:space-y-8 overflow-hidden">
+      <h2 className="font-medium text-xl">Invest</h2>
+      <div className="flex w-full justify-center">
+        <ScrollArea className=" max-sm:w-[17rem] lg:w-[50rem] whitespace-nowrap rounded-lg border bg-snow shadown-md">
+          <div className="flex w-max space-x-4 p-4">
+            <Invest />
+          </div>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+      </div>
+    </div>
+  );
 }
