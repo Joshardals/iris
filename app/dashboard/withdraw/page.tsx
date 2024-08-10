806994868412b72f4e68d6cef7a728bf0158ff7a
Joshardals <joshuabamidele219@gmail.com>
2024-08-10 16:58:15 +0100

    done with withdrawal page

--- app/dashboard/withdraw/page.tsx
+++ app/dashboard/withdraw/page.tsx
@@ -1,5 +1,3 @@
-import { Address } from "../_components/Withdraw/Address";
-import { convertAmount } from "@/lib/utils";
 import {
   fetchCurrentUserAccountInfo,
   fetchCurrentUserWalletInfo,
@@ -23,26 +21,6 @@ export default async function WithdrawPage() {
         </p>
 
         <Exchange />
-
-        {/* <div className="border-x border-x-onyx/50 border-t border-t-onyx/50 w-full overflow-x-auto rounded-lg">
-          <div className="grid md:grid-cols-3 md:gap-4 border-b border-b-onyx/50 max-md:py-2 max-md:px-5 max-md:space-y-2">
-            <p className="md:border-r md:border-r-onyx/50 md:px-5 md:py-2">
-              Portfolio:
-            </p>
-            <p className="font-semibold md:py-2 col-span-2">
-              {convertAmount(accountInfo?.accountBalance)}
-            </p>
-          </div>
-
-          <Address label="BITCOIN" address={walletInfo?.bitcoinWallet} />
-          <Address label="ETHEREUM" address={walletInfo?.ethereumWallet} />
-          <Address label="DOGECOIN" address={walletInfo?.dogeWallet} />
-          <Address label="LITECOIN" address={walletInfo?.litecoinWallet} />
-          <Address label="TRON" address={walletInfo?.tronWallet} />
-          <Address label="SHIBA" address={walletInfo?.shibaWallet} />
-          <Address label="USDT" address={walletInfo?.usdtWallet} />
-        </div> */}
-
         <WithdrawForm accountBalance={accountInfo?.accountBalance} />
       </div>
     </div>
