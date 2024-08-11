17a2b51c4e7008431d1012233bb3e959ca179acf
Joshardals <joshuabamidele219@gmail.com>
2024-08-11 14:30:15 +0100

    initial commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,8 +2,11 @@ import { Header } from "../_components/Header";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Iris Investment | Sign Up",
+  title: "Iris Investment | Login",
   description: "Create your login for Iris Investment",
+  openGraph: {
+    images: "https://i.ibb.co/qyxDtDN/localhost-3000.png",
+  },
 };
 
 export default function AuthLayout({
